0a5724f1c239208568eb3170240b371254c1ca1b
Jorge Rodriguez <102831018+JorgeRSLC@users.noreply.github.com>
2024-06-06 08:00:45 -0600

    Create product detail child component for catalog component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { SiteHeaderComponent } from './site-header/site-header.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
 
@@ -14,6 +15,7 @@ import { SiteHeaderComponent } from './site-header/site-header.component';
     HomeComponent,
     CatalogComponent,
     SiteHeaderComponent,
+    ProductDetailsComponent,
   ],
   imports: [
     BrowserModule
